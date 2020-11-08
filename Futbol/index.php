6155f15f96c3bce399ac9d5ab495d958f4fa402c
roldanordonez1990 <roldanordonez.francisco@gmail.com>
2020-11-08 17:03:33 +0100

    first

---
+++ Futbol/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <h3>Elige una opción de las que aparecen</h3>
+        
+        <ul>     
+            <li><a href="IntroducirDatos.php">1.- Introducir</a></li>
+            <li><a href="MostrarDatos.php">2.- Mostrar Datos</a></li>
+            <li><a href="BuscarDatos.php">3.- Buscar</a></li>
+            <li><a href="ModificarDatos.php">4.- Modificar</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
