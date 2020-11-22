a3e2c1ba76a07a3d6c0c7ff853e03e9be010f6b9
roldanordonez1990 <roldanordonez.francisco@gmail.com>
2020-11-22 02:31:24 +0100

    ejercicio 4

---
+++ Preferencias/Mostrar.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if(isset($_POST['borrar'])){
+    setcookie(session_name(), "", time()-3600, "/Preferencias");
+    session_unset();
+    session_destroy();
+    
+}
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 1</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Preferencias</legend>
+            <form action="" method="post">
+            <div class="campo">
+                <label class="etiqueta">Idioma:</label>
+                <br>
+                <label class="texto"><?php if (isset($_SESSION['idioma'])) echo $_SESSION['idioma'] ?></label>
+            </div>
+            <br>
+            <div class="campo">
+                <label class="etiqueta">Perfil público:</label>
+                <br>
+                <label class="texto"><?php if (isset($_SESSION['perfil'])) echo $_SESSION['perfil'] ?></label>
+            </div>
+            <br>
+            <div class="campo">
+                <label class="etiqueta">Zona horaria:</label>
+                <br>
+                <label class="texto"><?php if (isset($_SESSION['zona'])) echo $_SESSION['zona'] ?></label>
+            </div>
+            <br>
+            <input class="borrado" type="submit" name="borrar" value="Borrar preferencias">
+             <br>
+             
+             <a href="Preferencias.php">Establecer preferencias</a>
+            </form>
+        </fieldset>
+    </body>
+</html>
---
+++ Preferencias/Preferencias.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+//Creamos la sesión
+setcookie(session_name(), session_id(), time()+3600);
+
+if(isset($_POST['establecer'])){
+//Mientras la sesión permanece abierta, puedes utilizar la variable superglobal $_SESSION para añadir información a la sesión del usuario
+$_SESSION['idioma'] = $_POST['idioma'];
+$_SESSION['perfil'] = $_POST['perfil'];
+$_SESSION['zona'] = $_POST['zona'];
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>preferencias</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Preferencias</legend>
+            <?php
+            if(isset($_POST['establecer'])){
+               ?>
+                 <span class="mensaje">Información guardada en la sesión</span>
+               <?php
+            }
+            ?>
+           
+            <form action="" method="post">
+                <label class="etiqueta">Idioma:</label>
+                <select name="idioma">
+                   <?php
+                   $idiomas = ["Español", "Inglés"];
+                   foreach ($idiomas as $idioma){
+                       ?>
+                    <option value="<?php echo $idioma?>" <?php if(isset($_SESSION['idioma'])) if($_SESSION['idioma'] == $idioma) echo "selected"?>><?php echo $idioma;?></option>
+                    <?php
+                   }
+                   ?>
+                </select>
+                <br/>
+                <br/>
+                <label class="etiqueta">Perfil Público:</label>
+                <select name="perfil">
+                      <?php
+                   $perfiles = ["Si", "No"];
+                   foreach ($perfiles as $perfil){
+                       ?>
+                    <option value="<?php echo $perfil?>" <?php if(isset($_SESSION['perfil'])) if($_SESSION['perfil'] == $perfil) echo "selected"?>><?php echo $perfil;?></option>
+                    <?php
+                   }
+                   ?>
+                </select>
+                <br/>
+                <br/>
+                <label class="etiqueta">Zona horaria:</label>
+                <select name="zona">
+                    <?php
+                   $zonas = ["GMT-2", "GMT-1", "GMT", "GMT+1", "GMT+2"];
+                   foreach ($zonas as $zona){
+                       ?>
+                    <option value="<?php echo $zona?>"  <?php if(isset($_SESSION['zona'])) if($_SESSION['zona'] == $zona) echo "selected"?>><?php echo $zona;?></option>
+                    <?php
+                   }
+                   ?>
+                </select>
+                <br/>
+                <br/>
+                <input type="submit" name="establecer" value="Establecer Preferencias">
+                <br/>
+                <br/>
+                <a href="Mostrar.php">Mostrar preferencias</a>
+
+            </form>
+        </fieldset>
+    </body>
+</html>
